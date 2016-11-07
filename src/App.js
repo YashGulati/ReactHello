be2904df5eb3c7b1f47e377bbce2bbbb28137c2a
George Tony <gtony@suyati.com>
2016-11-07 14:57:35 +0530

    InputName

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
-class App extends Component {
+class InputName extends Component {
   constructor(props) {
      super(props);
      // above statement is necessary to run
      this.state = {name: ""};
    }
+   render(){
+     return (
+       <div>
+        <input value={this.state.name} onChange={ e => this.setState({ name : e.target.value})} />
+        <br/>
+        Hello {this.state.name}
+       </div>
+     )
+   }
+}
+class App extends Component {
+
   render() {
     return (
       <div className="App">
@@ -16,18 +27,15 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          <input value={this.state.name} onChange={ e => this.setState({ name : e.target.value})}/>
-          <br/>
-          Hello {this.state.name}
-          {/*
-
-            On Next Task Input Tag and Hello{this.state.name} to be in class InputName
-
-          */}
+            <InputName />
         </p>
       </div>
     );
   }
 }
+/*
 
+Next Task pass Default name to Input Name from App to InputName as props 
+
+*/
 export default App;
