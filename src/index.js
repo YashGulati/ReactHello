8394ac27907f86b6efbc4cbec62e21aac4602c6c
George Tony <gtony@suyati.com>
2016-11-11 12:07:36 +0530

    router in package.json

--- src/index.js
+++ src/index.js
@@ -2,8 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
 import './index.css';
-
+import { Router, Route,  browserHistory } from 'react-router'
+let route = (
+  <Router history={browserHistory}>
+    <Route path="/" component={App}/>
+  </Router>
+)
 ReactDOM.render(
-  <App />,
+  route,
   document.getElementById('root')
 );
